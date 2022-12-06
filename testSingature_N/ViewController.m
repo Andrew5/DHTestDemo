a6b38d63b901eb38b8418aa4058b8eaa2fa89fb4
Andrew5 <localhost3585@gmail.com>
2022-12-06 18:11:24 +0800

    chore: 动态获取属性 类型，获取状态栏、安全告诉等

--- testSingature_N/ViewController.m
+++ testSingature_N/ViewController.m
@@ -223,7 +223,7 @@ static inline CGRect XWRectMake(CGFloat x, CGFloat y, CGFloat width, CGFloat hei
         [YYFPSLabel xw_addFPSLableOnWidnow];
     });
 //    [MasonryAutoViewController d_release];
-//    [self test14];
+    [self test14];
     NSString *resultStr = @"childSelectedBtn";
     if(resultStr && resultStr.length>0) {
         resultStr = [resultStr stringByReplacingCharactersInRange:NSMakeRange(0,1) withString:[[resultStr substringToIndex:1] capitalizedString]];
@@ -232,14 +232,15 @@ static inline CGRect XWRectMake(CGFloat x, CGFloat y, CGFloat width, CGFloat hei
     NSLog(@"空%@",resultStr);
     NSLog(@"属性 %@",KEY_PATH(self,alertViewCustom));
     
-    UIButton*targetBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    targetBtn.frame = CGRectMake(0, 100, 50,50);
-    targetBtn.tag =100;
-    [targetBtn setTitle:@"点我"forState:UIControlStateNormal];
-    [targetBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [targetBtn addTarget:self action:@selector(targetBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
-    [targetBtn setInfo:@{@"id":@"12138",@"name":@"target-action"}];
-    [self.view addSubview:targetBtn];
+//    UIButton*targetBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+//    targetBtn.frame = CGRectMake(0, 100, 50,50);
+//    targetBtn.tag =100;
+//    [targetBtn setTitle:@"点我"forState:UIControlStateNormal];
+//    [targetBtn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+//    [targetBtn addTarget:self action:@selector(targetBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+//    [targetBtn setInfo:@{@"id":@"12138",@"name":@"target-action"}];
+//    [self.view addSubview:targetBtn];
+
 }
 - (void)targetBtnClicked:(UIButton*)sender {
     NSLog(@"我被点击了");
@@ -603,7 +604,9 @@ static inline CGRect XWRectMake(CGFloat x, CGFloat y, CGFloat width, CGFloat hei
 //    NSArray *array = [NSArray arrayWithObjects:@"324",@"3241", nil];
 //    NSString *testString = array[2];
 //    NSLog(@"测试结果 %@",testString);
-
+    NSData *data = [NSData dataWithBytes:(unsigned char []){0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x42, 0x61, 0x72} length:9];
+    NSString *key = [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+    SEL medth = NSSelectorFromString(key);
     // 约定好的规则(只针对push做操作)
     NSDictionary *params = @{
                              @"class" : @"YDYClaimGoalCustomerRequestInfoManageVC",
--- testSingature_N/ViewController/JumpRult/DHTJumpControllerTool.m
+++ testSingature_N/ViewController/JumpRult/DHTJumpControllerTool.m
@@ -64,16 +64,76 @@
 
 // 获取当前显示在屏幕最顶层的 ViewController
 + (UIViewController *)topViewController {
-
+   
     UIWindow * window = nil;
     if (@available(iOS 13.0, *)) {
         for (UIWindowScene* windowScene in [UIApplication sharedApplication].connectedScenes) {
             if (@available(iOS 15.0, *)) {
                 NSLog(@"%@",windowScene.keyWindow);
+                unsigned int count;
+                // 属性
+                Ivar *ivarList = class_copyIvarList([[UIApplication sharedApplication] class], &count);
+                // 成员变量
+                objc_property_t *propertyList = class_copyPropertyList([windowScene.statusBarManager class] , &count);
+                
+                for (int i =0; i < count; i++) {
+                    objc_property_t property = propertyList[i];
+                    // 成员变量列表
+                    NSString *nameStr = [NSString stringWithUTF8String:ivar_getName(ivarList[i])];
+                    NSLog(@"***ivarName:%@", nameStr);
+                    // 属性
+                    const char *propertyName = property_getName(property);
+                    NSLog(@"propertyName(%d) : %@", i, [NSString stringWithUTF8String:propertyName]);
+                    //属性特性
+                    const char *attributes = property_getAttributes(property);
+                    NSString *attributesStr = [NSString stringWithCString:attributes encoding:NSUTF8StringEncoding];
+                    NSLog(@"***attributes:%@", attributesStr);
+                    
+                    fprintf(stdout, "C语言 %s %s\n", property_getName(property), property_getAttributes(property));
+
+                    // 获取属性所属类名
+//                    unsigned int attrCount;
+//                    NSString *propertyType;
+//                    objc_property_attribute_t *attrs = property_copyAttributeList(property, &attrCount);
+//                    NSString *attrs111 = [NSString stringWithCString:&attrs[i].name[0] encoding:NSUTF8StringEncoding];
+//                    NSLog(@"%@",attrs111);
+                    if ([nameStr isEqualToString:@"_statusBar"]) {
+                        Ivar ivar = ivarList[i];
+                        NSLog(@"%s", ivar_getTypeEncoding(ivar));
+                        // 获得成员变量的类型
+                        NSString *type = [NSString stringWithUTF8String:ivar_getTypeEncoding(ivar)];
+                        // 如果属性是对象类型
+                        NSRange range = [type rangeOfString:@"@"];
+                        if (range.location != NSNotFound) {
+                            type = [type substringWithRange:NSMakeRange(2, type.length - 3)];
+                            // 排除系统的对象类型
+                            if (![type hasPrefix:@"NS"]) {
+                                // 将对象名转换为对象的类型，将新的对象字典转模型（递归）
+                                Class class = NSClassFromString(type);
+                                NSLog(@"%@",class);
+                                id viewController = [[class alloc] init];
+                                NSLog(@"statusBar：%@",  [viewController objectForKey:@"statusBar"]);
+                                
+                                unsigned int count11;
+                                Ivar *ivarList1 = class_copyIvarList(class, &count11);
+                                objc_property_t *propertyList = class_copyPropertyList(class , &count11);
+                                for (int i =0; i < count11; i++) {
+                                    objc_property_t property = propertyList[i];
+                                    fprintf(stdout, "C语言 %s %s\n", property_getName(property), property_getAttributes(property));
+                                    NSLog(@"成员变量列表**ivarName:%@", [NSString stringWithUTF8String:ivar_getName(ivarList1[i])]);
+                                }
+                            }
+                        }
+                    } else {
+                        
+                    }
+                }
+                free(ivarList);
+                free(propertyList);
                 UIStatusBarManager *statusBarManager = windowScene.statusBarManager;
-                NSLog(@"状态栏包括安全区:%f",statusBarManager.statusBarFrame.size.height);
-                NSLog(@"顶部安全区高度:%f",windowScene.windows.firstObject.safeAreaInsets.top);
-                NSLog(@"底部安全区高度:%f",windowScene.windows.firstObject.safeAreaInsets.bottom);
+                NSLog(@"状态栏包括安全区:%f",statusBarManager.statusBarFrame.size.height);//54
+                NSLog(@"顶部安全区高度:%f",windowScene.windows.firstObject.safeAreaInsets.top);//59
+                NSLog(@"底部安全区高度:%f",windowScene.windows.firstObject.safeAreaInsets.bottom);//34
             } else {
                 NSLog(@"%@",windowScene.windows);
                 // Fallback on earlier versions
@@ -101,6 +161,43 @@
     return resultVC;
 }
 
+- (void)setDict:(NSDictionary *)dict {
+    Class c = self.class;
+    while (c &&c != [NSObject class]) {
+        unsigned int outCount = 0;
+        Ivar *ivars = class_copyIvarList(c, &outCount);
+        for (int i = 0; i < outCount; i++) {
+            Ivar ivar = ivars[i];
+            NSString *key = [NSString stringWithUTF8String:ivar_getName(ivar)];
+            // 成员变量名转为属性名（去掉下划线 _ ）
+            key = [key substringFromIndex:1];
+            // 取出字典的值
+            id value = dict[key];
+            // 如果模型属性数量大于字典键值对数理，模型属性会被赋值为nil而报错
+            if (value == nil) continue;
+            // 获得成员变量的类型
+            NSString *type = [NSString stringWithUTF8String:ivar_getTypeEncoding(ivar)];
+            // 如果属性是对象类型
+            NSRange range = [type rangeOfString:@"@"];
+            if (range.location != NSNotFound) {
+                // 那么截取对象的名字（比如@"Dog"，截取为Dog）
+                type = [type substringWithRange:NSMakeRange(2, type.length - 3)];
+                // 排除系统的对象类型
+                if (![type hasPrefix:@"NS"]) {
+                    // 将对象名转换为对象的类型，将新的对象字典转模型（递归）
+                    Class class = NSClassFromString(type);
+                    NSLog(@"%@",class);
+                    //                    value = [class objectWithDict:value];
+                }
+                
+            }
+            // 将字典中的值设置到模型上[self setValue:value forKeyPath:key];
+            
+        }
+        free(ivars);
+        c = [c superclass];
+    }
+}
 + (UIWindow *)getRootWindow {
 
     NSArray *windows = [[UIApplication sharedApplication] windows];
